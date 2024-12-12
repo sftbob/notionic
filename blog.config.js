0b56e9edc430e74ad81ee688008117d7435c1d66
sftbob <111048941+sftbob@users.noreply.github.com>
2024-12-12 20:37:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,18 +1,18 @@
 const BLOG = {
-  title: 'Notionic',
-  author: '左蓝',
-  email: 'i@zuolan.me',
-  link: 'https://zuolan.me',
-  newsletter: 'Notionic Weekly',
-  description: 'A static blog build on top of Notion and Next.js',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  timezone: 'Asia/Shanghai', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
+  title: '新店誌',
+  author: '文人阿哖',
+  email: 'sftbob2@gmail.com',
+  link: 'https://notionik.vercel.app/',
+  newsletter: '夏日專題',
+  description: '生活.新店.日誌.',
+  lang: 'zh-TW', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  timezone: 'Asia/Taipei', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#F6F8FA', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#212936', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Notionic in a folder
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2024, // If leave this empty, current year will be used.
   postsPerPage: 10,
   sortByDate: true,
   pagesShow: {
@@ -22,6 +22,7 @@ const BLOG = {
     contact: true,
     books: true,
     friends: true
+    movies:true
   },
   showWeChatPay: true,
   previewImagesEnabled: true,
